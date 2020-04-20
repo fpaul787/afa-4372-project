c7bee36e29ab423807ea1b0dde534664ae0eb0aa
Frantz <fpaul787@gmail.com>
2020-04-20 01:01:25 -0400

    final verion of app

--- src/pages/assignment/index.md
+++ src/pages/assignment/index.md
@@ -4,4 +4,5 @@ date: "2020-04-20"
 title: "My Summative 2 Solution"
 author: "Frantz Paul"
 ---
-This is a test
+This is a test. My poem is going to be 
+very long and I want people to know it.
--- src/pages/solution.js
+++ src/pages/solution.js
@@ -2,17 +2,52 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from '../components/layout'
 
-const Page = ({data}) => (
+export default function Page({data}) {
+        
+    const post = data.allMarkdownRemark.edges[0]
+
+    return (
     <div>
         <Layout>
-        <h1>Hi</h1>
+            
+            <hr/>
+            <h1>Assignment</h1>
+            <div >
+            <h3>{post.node.frontmatter.title}</h3>
+            <small>Posted by {post.node.frontmatter.author} </small>
+            <br/>
+            <br/>
+            
+            <div dangerouslySetInnerHTML={{__html: post.node.html}}/>
+            <br/>
+            <br/>
+            <hr/>
+          </div>
+      
         </Layout>
         
     </div>
-)
+    )
+    
+}
 
-export const pagQuery = graphql`
-
+export const pageQuery = graphql`
+query SolutionPostByPath{
+    allMarkdownRemark{
+        edges{
+          node{
+            id
+            html
+            frontmatter{
+              path
+              title
+              date
+              author
+            }
+            excerpt
+          }
+        }
+      }
+}
 `
 
-export default Page
